fa196339c1072abdce795138943727ec85b5c9c0
Prince kumar <112043341+cutexboy@users.noreply.github.com>
2023-12-29 15:02:26 +0530

    Delete console.py

--- console.py
+++
@@ -1,5 +0,0 @@
-from PMBOT.console import LOGGER
-from PMBOT.modules.core import PMBOT
-from PMBOT.modules.vars import Config
-from PMBOT.modules.utils import commandx
-from PMBOT.modules.utils import commandz
